3bd306f349169b0fb957b47785b83f4e9f9dab40
RAIHAN UDDIN <raihanuddin@RAIHANs-MacBook-Pro.local>
2024-11-12 15:06:40 +0600

    error resolved

--- next.config.js
+++ next.config.js
@@ -1,32 +1,32 @@
-// /** @type {import('next').NextConfig} */
-// // const isProd = process.env.NODE_ENV === 'production';
-
-// const nextConfig = {
-//     // logging: {
-//     //     fetches: {
-//     //         fullUrl: true,
-//     //     },
-//     // },
-//     // assetPrefix: isProd ? '/Prossity/' : '', // Adjust for your GitHub Pages repo name
-//     // basePath: isProd ? '/Prossity' : '',    // Adjust for your GitHub Pages repo name
-//     // trailingSlash: true,                    // Ensures that static assets are correctly referenced
-//     // images: {
-//     //     unoptimized: true,                  // GitHub Pages doesn't support image optimization
-//     // },
-//     // output: 'standalone',  // This is important for Vercel to handle dynamic routes
-
-// };
-
-// module.exports = nextConfig;
-
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     logging: {
         fetches: {
             fullUrl: true,
         },
     },
-}
+    assetPrefix: isProd ? '/Prossity/' : '', // Adjust for your GitHub Pages repo name
+    basePath: isProd ? '/Prossity' : '',    // Adjust for your GitHub Pages repo name
+    trailingSlash: true,                    // Ensures that static assets are correctly referenced
+    images: {
+        unoptimized: true,                  // GitHub Pages doesn't support image optimization
+    },
+    output: 'standalone',  // This is important for Vercel to handle dynamic routes
 
-module.exports = nextConfig
+};
+
+module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     logging: {
+//         fetches: {
+//             fullUrl: true,
+//         },
+//     },
+// }
+
+// module.exports = nextConfig
 
